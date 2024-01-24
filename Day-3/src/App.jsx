5bf175ce70c24c7a6f512e4c171b668f164fb33d
Harish-3569 <118507612+Harish-3569@users.noreply.github.com>
2024-01-24 15:06:40 +0530

    Add files via upload

---
+++ Day-3/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Signin from './component/Signin'
+import Signup from './component/Signup'
+import Home from './component/Home'
+import Intro from './component/Intro'
+const App = () => {
+  return (
+    <div>
+      {/* <Signin /> */}
+      {/* <Signup /> */}
+      <Home />
+      {/* <Intro /> */}
+    </div>
+  )
+}
+
+export default App
---
+++ Day-3/src/component/Signup.jsx
@@ -0,0 +1,34 @@
+import './Signin'
+const Signup = () => {
+  return (
+    <div className='whole'>
+        
+    <div className='signin'>
+        <center>EVENT MANAGER</center>
+        <h2><center>Sigup:</center></h2>
+        <form>
+            Name:
+            <br></br>
+            <input></input>
+            <br></br>
+            Email:
+            <br></br>
+            <input type='email'></input>
+            <br></br>
+            Date Of Birth:
+            <br></br>
+            <input type='date'></input>
+            <br></br>
+            Role:
+            <br></br>
+            <br></br>
+            <br></br>
+            <center><button type='submit' className='button-85'>Submit</button></center>
+        </form>
+        </div>
+        </div>
+
+  )
+}
+
+export default Signup
